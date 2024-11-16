93446604c19e2282212b0d31e89720b852d81911
Giuseppe <fixercrade@hotmail.it>
2024-11-16 04:47:20 +0100

    first version of login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import path from 'node:path';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
 
-    {path: '', component: LandingPageComponent} 
+    {path: '', component: LandingPageComponent},
+    {path: 'login', component: LoginComponent}
     
 ];
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { FootbarComponent } from "../footbar/footbar.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FootbarComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
